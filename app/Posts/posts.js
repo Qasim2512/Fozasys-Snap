a7a817fcc5778db30e9e73f120ee1376e877d19a
Qasim2512 <122743097+Qasim2512@users.noreply.github.com>
2025-03-31 23:06:40 +0200

    fixes

--- app/Posts/posts.js
+++ app/Posts/posts.js
@@ -1,22 +1,34 @@
 /** @format */
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import styles from "./Posts.style";
 import SearchBar from "../../Components/Searchbar/Searchbar";
 
 const Posts = () => {
-  useEffect(async () => {
-    let response = await fetch("http://localhost:3000/photo");
-    let json = await response.json();
-    console.log(json);
-    return json;
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetchData();
   }, []);
+
+  const fetchData = async () => {
+    let response = await fetch("http://localhost:3000/photo");
+    let data = await response.json();
+    setPhotos(data);
+  };
+
+  console.log(photos);
 
   return (
     <View style={styles.container}>
       <SearchBar style={styles.searchbar} />
       <Text>Her skal alle postene ligge</Text>
+      {photos.length > 0 ? (
+        <Text>Navn: {photos[0].photo}</Text>
+      ) : (
+        <Text>Loading...</Text>
+      )}
     </View>
   );
 };
