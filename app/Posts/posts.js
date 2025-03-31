c61f1a70557bcf04123490c5da0888ab5f2500df
Qasim2512 <122743097+Qasim2512@users.noreply.github.com>
2025-03-31 22:24:22 +0200

    koblet til backend, ekte

--- app/Posts/posts.js
+++ app/Posts/posts.js
@@ -1,14 +1,21 @@
 /** @format */
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 import styles from "./Posts.style";
 import SearchBar from "../../Components/Searchbar/Searchbar";
 
 const Posts = () => {
+  useEffect(async () => {
+    let response = await fetch("http://localhost:3000/photo");
+    let json = await response.json();
+    console.log(json);
+    return json;
+  }, []);
+
   return (
     <View style={styles.container}>
-      <SearchBar style={styles.searchbar}/>
+      <SearchBar style={styles.searchbar} />
       <Text>Her skal alle postene ligge</Text>
     </View>
   );
